abec3345ff4ace788416aacb7be4c08734549d8f
Ivan <githubh@gmail.com>
2016-03-31 21:55:37 +0300

    Update items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,13 @@ class ItemsController < ApplicationController
     @items = Item.all
     render text: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>") 
   end
+
+  def show
+    @item = Item.create(name: params[:name], description: params[:description], price: params[:price], real: params[:real], weight: params[:weight])
+    render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
+  end
+
+
+
+
 end
