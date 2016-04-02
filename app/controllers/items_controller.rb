1bc57f88800c747cbfeeb64f87a05b8680969415
Ivan <githubh@gmail.com>
2016-04-02 16:12:42 +0300

    Add private method for items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,16 +7,20 @@ def index
 def show
   unless @item = Item.where(id: params[:id]).first
   render text: "Page not found", status: 404
-end
+  end
 end
 
 def new
+
 end
 
 def edit 
 end
 
 def create
+  @item = Item.new(item_params)
+  @item.save
+  redirect_to @item
 end
 
 def update
@@ -25,5 +29,10 @@ end
 def destroy
 end
 
+private
+
+  def item_params
+    params.require(:item).permit(:price, :name, :description, :weight)
+  end
 
 end
